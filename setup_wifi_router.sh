fde57ce45aaa3f16cd8382877749cd3c1a0606fb
SamiFarhat <sfarhat4@gmail.com>
2024-01-25 22:36:01 -0500

    Added echo comments

--- setup_wifi_router.sh
+++ setup_wifi_router.sh
@@ -1,22 +1,28 @@
+
 #!/bin/bash
 
 # Check for root privileges
+echo "Checking for root privileges..."
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
 # Update and upgrade the system
+echo "Updating and upgrading the system..."
 apt update && apt full-upgrade -y
 
 # Install necessary packages
+echo "Installing necessary packages..."
 apt install hostapd dnsmasq -y
 
 # Disable services to prevent them from starting after a reboot
+echo "Disabling services..."
 systemctl stop hostapd
 systemctl stop dnsmasq
 
 # Set up a static IP for wlan0
+echo "Setting up a static IP for wlan0..."
 cat >> /etc/dhcpcd.conf <<EOL
 
 interface wlan0
@@ -25,12 +31,14 @@ nohook wpa_supplicant
 EOL
 
 # Configure the DHCP server (dnsmasq)
+echo "Configuring the DHCP server (dnsmasq)..."
 cat > /etc/dnsmasq.conf <<EOL
 interface=wlan0
 dhcp-range=192.168.0.11,192.168.0.30,255.255.255.0,24h
 EOL
 
 # Configure the access point host software (hostapd)
+echo "Configuring the access point host software (hostapd)..."
 cat > /etc/hostapd/hostapd.conf <<EOL
 interface=wlan0
 driver=nl80211
@@ -49,21 +57,27 @@ rsn_pairwise=CCMP
 EOL
 
 # Tell the system where to find the configuration file
+echo "Updating hostapd configuration..."
 sed -i 's|#DAEMON_CONF=""|DAEMON_CONF="/etc/hostapd/hostapd.conf"|' /etc/default/hostapd
 
 # Enable IP forwarding
+echo "Enabling IP forwarding..."
 sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|' /etc/sysctl.conf
 
 # Add a masquerade for outbound traffic on eth0
+echo "Adding a masquerade for outbound traffic on eth0..."
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # Save the iptables rule
+echo "Saving the iptables rule..."
 sh -c "iptables-save > /etc/iptables.ipv4.nat"
 
 # Install iptables-persistent to save rules on boot
+echo "Installing iptables-persistent..."
 apt-get install iptables-persistent -y
 
 # Apply changes
+echo "Applying changes..."
 systemctl unmask hostapd
 systemctl enable hostapd
 systemctl start hostapd
