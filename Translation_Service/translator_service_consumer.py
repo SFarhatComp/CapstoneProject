7299b1ebf1b6571bb212be8de4b956cae31ac5d8
SamiFarhat <sfarhat4@gmail.com>
2024-02-07 23:07:27 -0500

    Modified maybe should be working

--- Translation_Service/translator_service_consumer.py
+++ Translation_Service/translator_service_consumer.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from threading import Thread
 import requests
 import pika
@@ -10,10 +10,11 @@ channel, exchange_name = None, None
 
 
 class TranslationConsumer:
-    def __init__(self, language):
+    def __init__(self, language , websocket):
         self.language = language
+        self.websocket = websocket
 
-    def translate(self, ch, method, properties, body):
+    async def translate(self, ch, method, properties, body):
         text_to_translate = body.decode()
 
         # Your data payload
@@ -32,13 +33,13 @@ class TranslationConsumer:
         if response.status_code == 200:
             # Parse the response
             translated_text = response.json()
+            await self.websocket.send("Your message here")
             print(translated_text)
             
         else:
             print("Failed to translate. Status code:", response.status_code)
 
         ch.basic_ack(delivery_tag=method.delivery_tag)
-
 
 
 def genereal_set_up():
@@ -53,30 +54,25 @@ def main(language: str = "fr"):
     global channel, exchange_name
     channel, exchange_name = genereal_set_up()
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    try:
-        channel.start_consuming()
     
-    except KeyboardInterrupt:
-        print("\nInterrupted by user. Stopping...")
-        channel.stop_consuming()
-  
-
-
-@app.get("/start")
-def start_consumer(language: str):
+@app.websocket("/ws/{language}")
+async def websocket_endpoint(websocket: WebSocket, language: str = "fr"):
+    await websocket.accept()
     # Start a new thread running the consumer for each request to this endpoint
-
     result = channel.queue_declare('', exclusive=True)
     queue_name = result.method.queue
     channel.queue_bind(exchange=exchange_name, queue=queue_name)
     channel.basic_qos(prefetch_count=1)
 
-   
-    consumer = TranslationConsumer(language)
+    consumer = TranslationConsumer(language, websocket)
     channel.basic_consume(queue=queue_name, on_message_callback=consumer.translate)
 
-    return {"message": "Consumer stated", "language": language}
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    try:
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        print("\nInterrupted by user. Stopping...")
+        channel.stop_consuming()
 
 if __name__ == "__main__":
     main()
