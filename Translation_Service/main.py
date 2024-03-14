9c4b6e0bd022f98110361e0477e64d4d99349342
Sami <sfarhat4@gmail.com>
2024-03-13 23:11:57 -0400

    Pi config

--- Translation_Service/main.py
+++ Translation_Service/main.py
@@ -23,4 +23,4 @@ async def websocket_endpoint(websocket: WebSocket, language: str = "fr"):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8001)
+    uvicorn.run(app, host="10.0.0.100", port=8001)
--- Translation_Service/translator_service_publisher.py
+++ Translation_Service/translator_service_publisher.py
@@ -20,7 +20,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -106,7 +106,7 @@ def main():
     global recognizer, stream , exchange_name, chanel
     recognizer, stream , exchange_name, chanel = general_set_up()
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="10.0.0.100", port=8000)
 
 if __name__== "__main__":
     main()
